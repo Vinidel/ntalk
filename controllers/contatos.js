f2bebc4d1c078dcd30aa9e333c61cdfeb81dbe17
Vinícius Deláscio <vinidroid@gmail.com>
2015-03-07 16:24:52 -0300

    Console.logs
    Puting some console.log to test the User and contacts jSon.

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -8,16 +8,26 @@ module.exports = function(app){
 			var params = {usuario: usuario,
 						  contatos: contatos};
 
+			console.log("Recuperou os contatos criados e incluiu no params:");
+			console.log(params);
+
+
 			res.render('contatos/index', params);
 		},
 
 		create: function(req, res){
 			console.log('entrou no Controller Contatos create');
 			var contato = req.body.contato;
-			var usuario = req.session.usuario;
-			
 
-			usuario.contatos.push(contato);	
+			console.log("Contato criado");
+			console.log("Nome: "  + contato.nome);
+			console.log("Email: "  + contato.email);
+
+			var usuario = req.session.usuario;
+
+			usuario.contatos.push(contato);
+			console.log("Array de contatos:");
+			console.log(usuario.contatos[0].nome);	
 
 			res.redirect('/contatos');
 		},
